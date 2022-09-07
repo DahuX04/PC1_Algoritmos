be662920b73ed86f5178a0dc6c95d101868ce54b
DahuX <siancas1908@hotmail.com>
2022-09-06 23:45:16 -0500

    c:

--- PC1_Algoritmos/Main.cpp
+++ PC1_Algoritmos/Main.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main() {
 	cout << "va funcionar";
-	cout << "que";
-	cout << "sakjbfa";
-	cout << "sjmafbnaf";
-
-	cout << "jamutaq ortega";
-
 	system("pause>0");
 	return 0;
 }
